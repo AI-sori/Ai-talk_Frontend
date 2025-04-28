10ed2bf3beebf3ca5412d138f7dba9404bc04378
yeondub1121 <kssjshyjj@naver.com>
2025-04-28 16:57:45 +0900

    ✨ Feat:   api 수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true, 
+    open: true,
+    proxy: {
+      '/members': {
+        target: 'https://port-0-ai-talk-backend-m95cvwb00db2ddb5.sel4.cloudtype.app',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 })
