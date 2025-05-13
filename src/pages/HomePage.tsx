a77e72a812783ce0b4ab9b95c7f8d5e2e97e69bc
yeondub1121 <kssjshyjj@naver.com>
2025-05-13 14:11:36 +0900

    fix

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { createHospitalOverlay } from '../utils/createHospitalOverlay';
 import HospitalList from '../components/HospitalList';
--- src/types/kakao.d.ts
+++ src/types/kakao.d.ts
@@ -1,11 +1,36 @@
+// src/types/kakao.d.ts
+export {}; // 모듈로 인식되도록 강제
+
 declare global {
   interface Window {
     kakao: any;
   }
 
   namespace kakao {
-    const maps: any;
+    namespace maps {
+      class Map {
+        constructor(container: HTMLElement, options: any);
+        setCenter(latlng: any): void;
+        setLevel(level: number): void;
+        // 필요 시 더 추가
+      }
+
+      class LatLng {
+        constructor(lat: number, lng: number);
+      }
+
+      class Marker {
+        constructor(options: any);
+        setMap(map: any): void;
+      }
+
+      class InfoWindow {
+        constructor(options: any);
+        open(map: any, marker: any): void;
+        close(): void;
+      }
+
+      // 등등 필요한 것들 추가
+    }
   }
 }
-
-export {};
