a6f23c2c04e56e3a30942715a276c01c5db8a303
yeondub1121 <kssjshyjj@naver.com>
2025-11-06 22:37:19 +0900

    Fix

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "/api", // Netlify가 VITE_API_URL로 프록시함
+  baseURL: import.meta.env.VITE_API_URL, 
   withCredentials: true,
-  headers: { "Content-Type": "application/json" },
+   headers: { "Content-Type": "application/json" } 
 });
 
 export default axiosInstance;
