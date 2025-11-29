7fc2556887f8d2c1447d3c50aba62ace85b1f008
yeondub1121 <kssjshyjj@naver.com>
2025-11-29 22:32:49 +0900

    Fix: 복구

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,20 +1,9 @@
 import axios from "axios";
-import useAuthStore from "../stores/useAuthStore";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
-});
-
-axiosInstance.interceptors.request.use((config) => {
-  const sessionId = useAuthStore.getState().user?.sessionId;
-
-  if (sessionId) {
-    config.headers["sessionId"] = sessionId;
-  }
-
-  return config;
 });
 
 export default axiosInstance;
