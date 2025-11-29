03dac521ffa0d5bb1826d431819e124ee908c578
yeondub1121 <kssjshyjj@naver.com>
2025-11-29 22:06:31 +0900

    Fix: 인스턴스도 수정

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,9 +1,21 @@
 import axios from "axios";
+import useAuthStore from "../stores/useAuthStore";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, 
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
-   headers: { "Content-Type": "application/json" } 
+  headers: { "Content-Type": "application/json" },
+});
+
+//세션 아이디 관련 추가
+axiosInstance.interceptors.request.use((config) => {
+  const { user } = useAuthStore.getState();
+
+  if (user?.sessionId) {
+    config.headers["sessionId"] = user.sessionId;   
+  }
+
+  return config;
 });
 
 export default axiosInstance;
